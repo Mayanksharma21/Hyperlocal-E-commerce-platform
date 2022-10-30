798a9f409b2a0e5f340ea319138cf576dedbefc1
Mayanksharma21 <mayank.sharmaa219@gmail.com>
2022-10-30 20:45:29 +0530

    Backend Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:eshop/views/splash_screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'content/consts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
