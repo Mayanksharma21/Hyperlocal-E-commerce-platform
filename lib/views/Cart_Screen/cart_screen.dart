f18b5406327760e1aa372b4db3fc680557e0f715
Mayanksharma21 <mayank.sharmaa219@gmail.com>
2022-10-30 13:01:14 +0530

    Cart Screen temporary added as an Empty cart screen

--- lib/views/Cart_Screen/cart_screen.dart
+++ lib/views/Cart_Screen/cart_screen.dart
@@ -3,9 +3,15 @@ import 'package:eshop/content/consts.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.purple,);
+    return Container(
+        color: Colors.white,
+        child: "your cart is empty!!"
+            .text
+            .color(darkFontGrey)
+            .fontFamily(bold)
+            .size(18)
+            .makeCentered());
   }
 }
